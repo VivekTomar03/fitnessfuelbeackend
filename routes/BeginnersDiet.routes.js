a57382885ead32e63a8ba66d445e76e3a44bb574
VivekTomar03 <tomarvivek90viv@gmail.com>
2023-05-13 16:46:19 +0530

    done

--- routes/BeginnersDiet.routes.js
+++ routes/BeginnersDiet.routes.js
@@ -1,24 +1,18 @@
-const express = require("express")
-const { BeginnersDietModel } = require("../model/BeginnersDiet.model")
+const express = require("express");
+const { BeginnersDietModel } = require("../model/BeginnersDiet.model");
 
-const beginnersDeitRouter = express.Router()
+const beginnersDeitRouter = express.Router();
+beginnersDeitRouter.get("/", async (req, res) => {
+  try {
+    const data = await BeginnersDietModel.find();
+    res.send(data);
+  } catch (error) {
+    res.send({
+      msg: error.messsage,
+    });
+  }
+});
 
-
-beginnersDeitRouter.get("/" , async(req,res) => {
-      try {
-        const data = await BeginnersDietModel.find()
-        res.send(data)
-      } catch (error) {
-        res.send({
-            msg:error.messsage
-        })
-      }
-})
-
-
-module.exports= {
-    beginnersDeitRouter
-}
-
-
-
+module.exports = {
+  beginnersDeitRouter,
+};
--- routes/beginnersExe.routes.js
+++ routes/beginnersExe.routes.js
@@ -1,23 +1,22 @@
-const express = require("express")
-const { BeginnersExeModel } = require("../model/beginnersExe.model")
-const beginnersRouter = express.Router()
+const express = require("express");
+const { BeginnersExeModel } = require("../model/beginnersExe.model");
+const beginnersRouter = express.Router();
 
+beginnersRouter.get("/", async (req, res) => {
+  try {
+    const data = await BeginnersExeModel.find();
+    res.send(data);
+  } catch (error) {
+    res.send({
+      msg: error.messsage,
+    });
+  }
+});
 
-beginnersRouter.get("/" , async(req,res) => {
-      try {
-        const data = await BeginnersExeModel.find()
-        res.send(data)
-      } catch (error) {
-        res.send({
-            msg:error.messsage
-        })
-      }
-})
+module.exports = {
+  beginnersRouter,
+};
 
-
-module.exports= {
-    beginnersRouter
-}
 
 
 
